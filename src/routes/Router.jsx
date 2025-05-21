8c1cf515867cf7589427b98ae6ec904166470498
Ayoosh <>
2025-05-21 14:08:05 +0300

    first commit

---
+++ src/routes/Router.jsx
@@ -0,0 +1,41 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Home from "../pages/Home";
+import Courses from "../pages/Courses"; 
+
+function Layout() {
+  return (
+    <>
+      {
+        < Home />
+    //   <Courses />
+
+
+      /* <Nav />
+      <Outlet />
+      <Footer /> */}
+    </>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "Courses", element: <Courses /> },
+    //   { path: "contact", element: <Contact /> },
+    //   { path: "Pages", element: <Pages /> },
+    ],
+  },
+]);
+
+function Router() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default Router;
